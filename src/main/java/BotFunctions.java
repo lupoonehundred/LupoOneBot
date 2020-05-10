d6c953ae951bdda210068ba23b4211009f41710b
Talhah Shaik <lupoonehundred@gmail.com>
2020-05-10 15:33:42 -0700

    Fixed the Verification process

--- src/main/java/BotFunctions.java
+++ src/main/java/BotFunctions.java
@@ -1,3 +1,5 @@
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
 import org.javacord.api.event.channel.user.PrivateChannelCreateEvent;
 import org.javacord.api.event.server.member.ServerMemberJoinEvent;
 import org.javacord.api.listener.channel.user.PrivateChannelCreateListener;
@@ -5,17 +7,19 @@ import org.javacord.api.listener.server.member.ServerMemberJoinListener;
 
 public class BotFunctions extends LupoOneBot implements ServerMemberJoinListener, PrivateChannelCreateListener {
     //FIXME Ultimately Optimize this section in total
+    private Server serverJoined;
     @Override
     public void onServerMemberJoin(ServerMemberJoinEvent event) { //When a new member joins the
+        serverJoined = event.getServer();
         loggerGetter().info("New member has Joined the Server: " +
-                serverGetter().toString() +
+                event.getServer().getName() + " " +
                 event.getUser().getDiscriminatedName());
         //Logs the name and server the user has joined.
         //FIXME Fix how the verification process works
 
         event.getUser().openPrivateChannel()
                 .thenAcceptAsync(channel -> {
-                    channel.sendMessage("Please Verify Who You Are. \n Reply With your Current Display Name. (Case Sensitive)");
+                    channel.sendMessage("Please Verify Who You Are. \nReply With your Current Display Name. (Case Sensitive)\nYou have 30 secs.");
                     loggerGetter().info("sent Verification to User: " + event.getUser().getDiscriminatedName());
                 });
         //Gets the user and asks for verification
@@ -27,6 +31,7 @@ public class BotFunctions extends LupoOneBot implements ServerMemberJoinListener
 
     @Override
     public void onPrivateChannelCreate(PrivateChannelCreateEvent event) {
+        if(serverJoined.getOwner().getDiscriminatedName().equals(event.getUser().getDiscriminatedName())) { return;}
         try {
             loggerGetter().info("Sleep Started.");
             Thread.sleep(30000);
@@ -49,7 +54,7 @@ public class BotFunctions extends LupoOneBot implements ServerMemberJoinListener
                 while(tries < 3) {
                     if(event.getUser().getName().equals(verify)) {
                         //event.getUser().addRole(vUser, "Verified User");
-                        event.getChannel().sendMessage("You have been Verified and can use the server." + serverGetter());
+                        event.getChannel().sendMessage("You have been Verified and can use the server." + serverJoined.getName());
                         loggerGetter().info("User " + event.getUser().getDiscriminatedName() + " has been Verified.");
                         //Tells the User that they have been verified and also logged it as well.
                         break;
@@ -74,6 +79,11 @@ public class BotFunctions extends LupoOneBot implements ServerMemberJoinListener
                     }
                     //place a try catch for them to retry the prompt.
                 }
+                User kUser = serverJoined.getMemberById(channel.getNewestMessage().get().getAuthor().getId()).get();
+                loggerGetter().info("User to be Kicked received: " + kUser.getDiscriminatedName());
+                serverJoined.kickUser(kUser);
+                loggerGetter().info("User has been Kicked: " + kUser.getDiscriminatedName());
+                event.getChannel().sendMessage("Verification failed and Removed from Server.");
             });
         //Send message to a User personally
         //TODO add an Exceptionally catcher
