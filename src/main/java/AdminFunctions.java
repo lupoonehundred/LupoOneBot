8a7bb1e0c50b4296b3ac467a473388fd9e7f0b63
Talhah Shaik <lupoonehundred@gmail.com>
2020-05-09 17:27:41 -0700

    Upload from project to Repo

--- src/main/java/AdminFunctions.java
+++ src/main/java/AdminFunctions.java
@@ -18,14 +18,18 @@ public class AdminFunctions extends LupoOneBot implements MessageCreateListener,
                     banEvent.getMessageAuthor().isServerAdmin()) {
                 loggerGetter().info("!ban received from an Admin "
                         + banEvent.getMessageAuthor().asUser().orElseThrow().getDiscriminatedName());
-                String id = banEvent.getMessageContent().substring(5);
-                banUserCommand(serverGetter(), id);
+                User bUser = event.getServer().get().getMemberById(event.getMessageContent().split(" ")[1]).get();
+                loggerGetter().info("User to be Banned received: " + bUser.getDiscriminatedName());
+                event.getServer().get().kickUser(bUser);
+                loggerGetter().info("User has been Banned: " + bUser.getDiscriminatedName());
             }
             else if(banEvent.getMessage().getContent().contains("!ban") &&
                     !banEvent.getMessageAuthor().isServerAdmin()) {
                 loggerGetter().info("!ban received from a NonAdmin " + banEvent.getMessageAuthor().asUser().toString());
-                String fakeID = banEvent.getMessageAuthor().getIdAsString();
-                banUserCommand(serverGetter(), fakeID);
+                User kUser = event.getServer().get().getMemberById(event.getMessage().getAuthor().getId()).get();
+                loggerGetter().info("User to be Banned received: " + kUser.getDiscriminatedName());
+                event.getServer().get().kickUser(kUser);
+                loggerGetter().info("User has been Banned: " + kUser.getDiscriminatedName());
             }
         });
         //Listens to a users ban
@@ -33,14 +37,18 @@ public class AdminFunctions extends LupoOneBot implements MessageCreateListener,
             if (kickEvent.getMessage().getContent().contains("!kick") &&
                     kickEvent.getMessageAuthor().isServerAdmin()) {
                 loggerGetter().info("!kick received from an Admin " + kickEvent.getMessageAuthor().asUser().toString());
-                String id = kickEvent.getMessageContent().substring(6);
-                kickUserCommand(serverGetter(), id);
+                User kUser = event.getServer().get().getMemberById(event.getMessageContent().split(" ")[1]).get();
+                loggerGetter().info("User to be Kicked received: " + kUser.getDiscriminatedName());
+                event.getServer().get().kickUser(kUser);
+                loggerGetter().info("User has been Kicked: " + kUser.getDiscriminatedName());
             }
             else if(kickEvent.getMessage().getContent().contains("!kick") &&
                     !kickEvent.getMessageAuthor().isServerAdmin()) {
                 loggerGetter().info("!kick received from a NonAdmin " + kickEvent.getMessageAuthor().asUser().toString());
-                String fakeID = kickEvent.getMessageAuthor().getIdAsString();
-                kickUserCommand(serverGetter(), fakeID);
+                User kUser = event.getServer().get().getMemberById(event.getMessage().getAuthor().getId()).get();
+                loggerGetter().info("User to be Kicked received: " + kUser.getDiscriminatedName());
+                event.getServer().get().kickUser(kUser);
+                loggerGetter().info("User has been Kicked: " + kUser.getDiscriminatedName());
             }
         });
         //Listens to a users kick
@@ -63,32 +71,6 @@ public class AdminFunctions extends LupoOneBot implements MessageCreateListener,
         });
         //Listens to a users unban
     }
-    //Similar things were placed in a new method for banning
-    private void banUserCommand(Server banServer, String receivedID) {
-        try {
-            User bUser = apiGetter().getUserById(receivedID).get();
-            loggerGetter().info("User to be Banned received: " + bUser.getDiscriminatedName());
-            banServer.banUser(bUser);
-            //FIXME These Commands are not working properly
-            loggerGetter().info("User has been Banned: " + bUser.getDiscriminatedName());
-        } catch (InterruptedException | ExecutionException ee) {
-            loggerGetter().error("Caught Ban User Exception: ", ee.getCause());
-            loggerGetter().error("ID received was " + receivedID);
-        }
-    }
-    //Similar things were placed in a new method kicking
-    private void kickUserCommand(Server kickServer, String receivedID) {
-        try {
-            User kUser = apiGetter().getUserById(receivedID).get();
-            loggerGetter().info("User to be Kicked received: " + kUser.getDiscriminatedName());
-            kickServer.kickUser(kUser);
-            //FIXME These Commands are not working properly
-            loggerGetter().info("User has been Kicked: " + kUser.getDiscriminatedName());
-        } catch (InterruptedException | ExecutionException ee) {
-            loggerGetter().error("Caught Kick User Exception: ", ee.getCause());
-            loggerGetter().error("ID received was " + receivedID);
-        }
-    }
 
     //Sends the Owner the User and Deleted Message.
     @Override
@@ -97,10 +79,8 @@ public class AdminFunctions extends LupoOneBot implements MessageCreateListener,
                 " \"" + event.getMessageContent().orElseThrow() + "\" Has been Deleted!");
         loggerGetter().info("Opening a Private Channel with User Admin: " + event.getServer().orElseThrow().getOwner().getDiscriminatedName());
         event.getServer().get().getOwner().openPrivateChannel()
-                .thenAcceptAsync(privateChannel -> {
-                    privateChannel.sendMessage("User " + event.getMessage().get().getAuthor().getDiscriminatedName()
-                    + " wrote \"" + event.getMessageContent().get() + "\" and the message got deleted.");
-                })
+                .thenAcceptAsync(privateChannel -> privateChannel.sendMessage("User " + event.getMessage().get().getAuthor().getDiscriminatedName()
+                + " wrote \"" + event.getMessageContent().get() + "\" and the message got deleted."))
                 //Tries to send a message to the Owner
                 .exceptionally(throwable -> {
                     loggerGetter().error("Sending Deleted Message to Owner Error " + throwable.getCause());
